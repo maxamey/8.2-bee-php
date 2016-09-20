2e12feddc663807ab9f261c2146e5036b32fe6df
Max Amey <maxwellamey@gmail.com>
2016-09-20 15:24:28 -0500

    Added a folder/file for main.js and a file for main.css in public. Then linked to them in the head of reasources/views/vendor/calculator.blade.php

---
+++ public/javascripts/main.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(){
+  var display = document.querySelector(".display");
+  var operators = document.querySelectorAll(".operators");
+  var keys = document.querySelectorAll(".keys");
+  var clear = document.querySelector(".clear");
+  var expression = "";
+  var historyCurrent = "";
+  var history = [];
+
+
+
+  keys.forEach(function(key){
+    key.addEventListener("click", function(e){
+    console.log("clicked:" + key.textContent);
+    var numberAdd = e.target.textContent;
+    display.textContent += numberAdd;
+    expression += numberAdd;
+    historyCurrent += numberAdd;
+  });
+});
+
+  clear.addEventListener("click", function(e){
+    display.textContent = "";
+    expression = "";
+    //clears current history
+    historyCurrent = "";
+  });
+
+  operators.forEach(function(operator){
+    operator.addEventListener("click", function(e){
+      console.log("clicked:" + operator.textContent);
+      var operateDisplay = e.target.textContent;
+      if(e.target.textContent === "="){
+        display.textContent = eval(expression);
+        historyCurrent += "=" + eval(expression);
+        //adds the current string of history to an array
+        history.push(historyCurrent);
+        console.log(history);
+      }else{
+        display.textContent += operateDisplay;
+        expression += operateDisplay;
+        historyCurrent += operateDisplay;
+      };
+    });
+  });
+});
---
+++ resources/views/calculator.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="<?php echo asset('main.css')?>" rel="stylesheet" type="text/css">
+        <script src="<?php echo asset('javascripts/main.js')?>" charset="utf-8"></script>
+    </head>
+    <body>
+      <body>
+  <h1 class="describe">this is not a calculator</h1>
+  <div class="calculator">
+    <div class="top">
+      <span class="clear">C</span>
+      <span class="display"></span>
+    </div>
+    <div class="bottom">
+      <!-- first row -->
+      <span class="keys">1</span>
+      <span class="keys">2</span>
+      <span class="keys">3</span>
+      <span class="operators">/</span>
+      <!-- second row -->
+      <span class="keys">4</span>
+      <span class="keys">5</span>
+      <span class="keys">6</span>
+      <span class="operators">*</span>
+      <!-- third row -->
+      <span class="keys">7</span>
+      <span class="keys">8</span>
+      <span class="keys">9</span>
+      <span class="operators">-</span>
+      <!-- fourth row -->
+      <span class="keys">0</span>
+      <span class="keys">.</span>
+      <span class="operators eval" >=</span>
+      <span class="operators">+</span>
+    </div>
+  </div>
+</body>
+    </body>
+</html>
